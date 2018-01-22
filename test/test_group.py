432b8d6e1ec2dc71ca85686344d5670be3735909
MSL <manuel.sierra@bluetab.net>
2018-01-22 16:54:10 +0100

    add dummy test

---
+++ test/test_group.py
@@ -0,0 +1,11 @@
+import unittest
+
+class GroupTestCase(unittest.TestCase):
+    """docstring for GroupTestCase."""
+
+    def test_dummytest(self):
+        """Test dummy"""
+        self.assertEqual(1, 1)
+
+if __name__ == "__main__":
+    unittest.main()
