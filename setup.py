0634435cfa55d802c718d51c68077ecdf09b5980
MSL <manuel.sierra@bluetab.net>
2018-01-30 13:21:50 +0100

    add fabric dependency

--- setup.py
+++ setup.py
@@ -11,10 +11,12 @@ setup(
     packages=['api', 'api.v1', 'api.common', 'api.settings', 'api.models'],
     include_package_data=True,
     install_requires=[
+        'Fabric3==1.13.1.post1',
         'Flask==0.12.2',
         'Flask-Cors==3.0.2',
         'Flask-HTTPAuth==3.2.3',
         'neo4j-driver==1.3.0',
+        'nose2==0.7.3',
         'nose2==0.7.3',
         'PyJWT==1.5.3',
         'requests==2.18.4',
