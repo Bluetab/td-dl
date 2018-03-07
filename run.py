ede111640437ffcf8160f9a23e5031ff1e09137a
MSL <manuel.sierra@bluetab.net>
2018-03-07 09:12:09 +0100

    update port

--- run.py
+++ run.py
@@ -7,5 +7,5 @@ from tornado.ioloop import IOLoop
 from api.app import app
 
 http_server = HTTPServer(WSGIContainer(app))
-http_server.listen(4002)
+http_server.listen(4003)
 IOLoop.instance().start()
