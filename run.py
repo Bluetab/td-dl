8787e4709f25773f6b6113276c244af5bffc9583
MSL <manuel.sierra@bluetab.net>
2018-03-14 18:48:31 +0100

    add autoreload tornado server

--- run.py
+++ run.py
@@ -1,11 +1,17 @@
 #!venv/bin/python
 # Run with tornado
+from tornado import autoreload
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 
 from api.app import app
 
+
+DEFAULT_APP_TCP_PORT = 4003
+
 http_server = HTTPServer(WSGIContainer(app))
-http_server.listen(4003)
-IOLoop.instance().start()
+http_server.listen(DEFAULT_APP_TCP_PORT)
+ioloop = IOLoop.instance()
+autoreload.start(ioloop)
+ioloop.start()
