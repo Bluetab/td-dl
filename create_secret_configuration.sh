e83b3fa38ca3642b354048b9574144210d24a1bf
MSL <manuel.sierra@bluetab.net>
2018-04-03 15:41:18 +0200

    fix app.py route

--- create_secret_configuration.sh
+++ create_secret_configuration.sh
@@ -2,4 +2,4 @@
 
 sed -i -e "s/NEO4J_PASSWORD = .*/NEO4J_PASSWORD = \"$NEO4J_PASSWORD\"/g" ./api/settings/config.py
 sed -i -e "s/SECRET_KEY = .*/SECRET_KEY = \"$GUARDIAN_SECRET_KEY\"/g" ./api/settings/config.py
-sed -i -e "s/app.config.from_object('api.settings.config.DevelopmentConfig')/app.config.from_object('api.settings.config.ProductionConfig')/g" .api/app.py
+sed -i -e "s/app.config.from_object('api.settings.config.DevelopmentConfig')/app.config.from_object('api.settings.config.ProductionConfig')/g" ./api/app.py
