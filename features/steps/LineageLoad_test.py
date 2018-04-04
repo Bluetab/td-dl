42c3aa17564dc3e310b965b5717508b744599010
MSL <manuel.sierra@bluetab.net>
2018-04-04 09:16:33 +0200

    fix test

--- features/steps/LineageLoad_test.py
+++ features/steps/LineageLoad_test.py
@@ -1,4 +1,4 @@
-from behave import *
+from behave import when, then, given
 from support import constants
 from support import lineage
 import requests
@@ -19,11 +19,11 @@ def step_impl(context, code):
 @then('"{username}" is able to view following "{type_analysis}" for Resource with Id "{resourceId}" and top level Group type "{groupType}"')
 def step_impl(context, username, type_analysis, resourceId, groupType):
     r = lineage.callAPIResources(context.token)
-    resource = lineage.findByKeyJson(r.json(), resourceId, 'external_id')
+    resource = lineage.findByKeyJson(r.json()["data"], resourceId, 'external_id')
 
     data = {"uuids":[resource['uuid']], "toplevel":groupType, "levels": -1, "type_analysis":type_analysis}
     r = requests.post(constants.API_PATH, data=json.dumps(data), headers=constants.get_header(context.token))
-    lineage.check_table_json(context, r.json()['paths'])
+    lineage.check_table_json(context, r.json()["data"]['paths'])
 
 @given('following lineage data has been already loaded')
 def step_impl(context):
@@ -35,7 +35,7 @@ def step_impl(context):
 @when('"{username}" tries to get the "{type_analysis}" analysis for Resource with Id "{resourceId}" and top level Group type "{groupType}"')
 def step_impl(context, username, type_analysis, resourceId, groupType):
     r = lineage.callAPIResources(context.token)
-    resource = lineage.findByKeyJson(r.json(), resourceId, 'external_id')
+    resource = lineage.findByKeyJson(r.json()["data"], resourceId, 'external_id')
 
     data = {"uuids":[resource['uuid']], "toplevel":groupType, "levels": -1, "type_analysis":type_analysis}
     r = requests.post(constants.API_PATH, json=data, headers=constants.get_header(context.token))
@@ -43,18 +43,18 @@ def step_impl(context, username, type_analysis, resourceId, groupType):
 
 @then('he receives following lineage information')
 def step_impl(context):
-    lineage.check_table_json(context, context.jsonData['paths'])
+    lineage.check_table_json(context, context.jsonData["data"]['paths'])
 
 @when('"{username}" tries to list Groups with a contains relation with Group Id "{group_id}"')
 def step_impl(context, username, group_id):
     r = lineage.callAPIGroups(context.token)
-    resource = lineage.findByKeyJson(r.json(), group_id, 'external_id')
+    resource = lineage.findByKeyJson(r.json()["data"], group_id, 'external_id')
     r = requests.get("{0}/{1}/index_contains".format(constants.API_GROUPS, resource['uuid']), headers=constants.get_header(context.token))
     context.jsonData = r.json()
 
 @then('he receives following nodes list')
 def step_impl(context):
-    lineage.check_table_json(context, context.jsonData)
+    lineage.check_table_json(context, context.jsonData["data"])
 
 @when('"{username}" tries to list top Groups')
 def step_impl(context, username):
--- features/support/lineage.py
+++ features/support/lineage.py
@@ -4,6 +4,7 @@ import pandas
 import jwt
 from api.app import app
 
+
 def createFilesAndUpload(context):
 
     df_groups = pandas.DataFrame([])
@@ -12,73 +13,100 @@ def createFilesAndUpload(context):
 
     for row in context.table:
         if row["File"] == 'Resources':
-            df_temp = pandas.DataFrame(data={'external_id:ID': [row["Id"]], \
-                'name': [row["Name"]], 'type': [row["Type"]], \
-                'description': [row["Description"]], \
+            df_temp = pandas.DataFrame(data={
+                'external_id:ID': [row["Id"]],
+                'name': [row["Name"]], 'type': [row["Type"]],
+                'description': [row["Description"]],
                 ':LABEL': ['Resource']})
             df_resources = appendDataFrame(df_resources, df_temp)
 
         elif row["File"] == 'Groups':
-            df_temp = pandas.DataFrame(data={'external_id:ID': [row["Id"]], \
-                'name': [row["Name"]], 'type': [row["Type"]], \
-                'description': [row["Description"]], \
-                'showtree': [row["Showtree"]], \
+            df_temp = pandas.DataFrame(data={
+                'external_id:ID': [row["Id"]],
+                'name': [row["Name"]], 'type': [row["Type"]],
+                'description': [row["Description"]],
+                'showtree': [row["Showtree"]],
                 ':LABEL': ['Group']})
             df_groups = appendDataFrame(df_groups, df_temp)
 
         elif row["File"] == 'Relations':
-            df_temp = pandas.DataFrame(data={':START_ID': [row["Id Source"]], \
+            df_temp = pandas.DataFrame(data={
+                ':START_ID': [row["Id Source"]],
                 ':END_ID': [row["Id Target"]], ':TYPE': [row["Type"]]})
             df_relations = appendDataFrame(df_relations, df_temp)
 
-    df_resources.to_csv(constants.FILENAME_RESOURCES, index = None, sep=';', encoding='utf-8')
-    df_groups.to_csv(constants.FILENAME_GROUPS, index = None, sep=';', encoding='utf-8')
-    df_relations.to_csv(constants.FILENAME_RELATIONS, index = None, sep=';', encoding='utf-8')
+    df_resources.to_csv(constants.FILENAME_RESOURCES,
+                        index=None, sep=';', encoding='utf-8')
+    df_groups.to_csv(constants.FILENAME_GROUPS,
+                     index=None, sep=';', encoding='utf-8')
+    df_relations.to_csv(constants.FILENAME_RELATIONS,
+                        index=None, sep=';', encoding='utf-8')
 
-    files = [(constants.NODES, open(constants.PATH + "/" + constants.FILENAME_GROUPS, 'rb')),
-        (constants.NODES, open(constants.PATH + "/" + constants.FILENAME_RESOURCES, 'rb')),
-        (constants.RELS, open(constants.PATH + "/" + constants.FILENAME_RELATIONS, 'rb'))]
+    files = [
+        (constants.NODES,
+            open(constants.PATH + "/" + constants.FILENAME_GROUPS, 'rb')),
+        (constants.NODES,
+            open(constants.PATH + "/" + constants.FILENAME_RESOURCES, 'rb')),
+        (constants.RELS,
+            open(constants.PATH + "/" + constants.FILENAME_RELATIONS, 'rb'))]
 
-    request = requests.post(constants.API_UPLOAD, files=files, headers=constants.get_auth_header(context.token))
+    request = requests.post(constants.API_UPLOAD,
+                            files=files,
+                            headers=constants.get_auth_header(context.token))
 
     return request
+
 
 def check_table_json(context, jsonData):
 
     for row in context.table:
         object_json = findByKeyJson(jsonData, row["Id"], 'external_id')
-        #assert False, "JSON: %s" % object_json
+        # assert False, "JSON: %s" % object_json
         assert object_json['name'] == row["Name"]
         assert object_json['type'] == row["Type"]
-        assert object_json['description'] == row["Description"], "JSON: %s" % object_json
+        assert object_json['description'] == row["Description"], \
+            "JSON: %s" % object_json
         if "Contains" in row and row["Contains"]:
-            findArrayJsonValues(context.token, object_json, 'contains', row["Contains"])
+            findArrayJsonValues(context.token,
+                                object_json, 'contains', row["Contains"])
         if "Depends" in row and row["Depends"]:
-            findArrayJsonValues(context.token, object_json, 'depends', row["Depends"])
+            findArrayJsonValues(context.token,
+                                object_json, 'depends', row["Depends"])
+
 
 def appendDataFrame(df, df_temp):
     df = df_temp if df.empty else df.append(df_temp)
     return df
 
+
 def findArrayJsonValues(token, object_json, findKey, findKeyFeatures):
     for valueJson in object_json[findKey]:
-        object_find = findByKeyJson(callAPIResources(token).json() + callAPIGroups(token).json(),
-                                    valueJson, 'uuid')
+        object_find = findByKeyJson(
+            callAPIResources(token).json()["data"] +
+            callAPIGroups(token).json()["data"], valueJson, 'uuid')
         try:
-            indexvalue = findKeyFeatures.split(",").index(object_find['external_id']), "Got %s" % object_find['external_id']
+            indexvalue = findKeyFeatures.split(",") \
+                                        .index(object_find['external_id']), \
+                                        "Got %s" % object_find['external_id']
+            assert indexvalue == object_find['external_id']
         except Exception as e:
-            assert False, "Value not find %s" %e
+            assert False, "Value not find %s" % e
+
 
 def callAPIResources(token):
-    r = requests.get(constants.API_RESOURCES, headers=constants.get_auth_header(token))
+    r = requests.get(constants.API_RESOURCES,
+                     headers=constants.get_auth_header(token))
     return r
+
 
 def callAPIGroups(token):
-    r = requests.get(constants.API_GROUPS, headers=constants.get_auth_header(token))
+    r = requests.get(constants.API_GROUPS,
+                     headers=constants.get_auth_header(token))
     return r
 
+
 def findByKeyJson(json, value, findKey):
-    object_json=None
+    object_json = None
 
     for x in json:
         if x[findKey] == value:
@@ -86,7 +114,10 @@ def findByKeyJson(json, value, findKey):
 
     return object_json
 
+
 def buildToken(user):
     user['user']['aud'] = app.config['JWT_AUD']
-    token = jwt.encode(user['user'], app.config['SECRET_KEY'], app.config['ALGORITHM']).decode("utf-8")
+    token = jwt.encode(user['user'],
+                       app.config['SECRET_KEY'],
+                       app.config['ALGORITHM']).decode("utf-8")
     return token
