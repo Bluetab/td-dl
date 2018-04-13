9c919aecd99c66dafdd2a2730eb6d8de1917e6a0
scuriel <sonia.curiel@bluetab.net>
2018-04-13 09:27:55 +0200

    publish swagger in /api/swagger

--- api/app.py
+++ api/app.py
@@ -37,7 +37,24 @@ swagger_template = {
     "security": [{ "bearer": []}]
 }
 
-Swagger(app, template=swagger_template)
+swagger_config = {
+    "headers": [
+    ],
+    "specs": [
+        {
+            "endpoint": 'apispec_1',
+            "route": '/apispec_1.json',
+            "rule_filter": lambda rule: True,  # all in
+            "model_filter": lambda tag: True,  # all in
+        }
+    ],
+    "static_url_path": "/flasgger_static",
+    # "static_folder": "static",  # must be set by user
+    "swagger_ui": True,
+    "specs_route": "/api/swagger"
+}
+
+Swagger(app, template=swagger_template, config=swagger_config)
 
 @app.errorhandler(404)
 def not_found(error):
