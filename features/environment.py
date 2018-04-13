648f416fc569285aa8531b146877eed34d18309e
scuriel <sonia.curiel@bluetab.net>
2018-04-13 09:17:55 +0200

    publish swagger in /api/swagger

--- features/environment.py
+++ features/environment.py
@@ -1,25 +1,22 @@
-from behave import fixture, use_fixture
+from multiprocessing import Process
 from tornado.httpserver import HTTPServer
 from tornado.wsgi import WSGIContainer
-from multiprocessing import Process
-from tornado.ioloop import IOLoop
-from tornado import autoreload
+from tornado import ioloop
 from api.app import app
 
-@fixture
-def flaskr_client(context, *args, **kwargs):
-
+def run_server():
     DEFAULT_APP_TCP_PORT = app.config['PORT']
-    #
-    # http_server = HTTPServer(WSGIContainer(app))
-    # http_server.listen(DEFAULT_APP_TCP_PORT)
-    # ioloop = IOLoop.instance()
-    #
-    #
-    # p = Process(target=autoreload.start(ioloop), args=())
-    # p.daemon = True                       # Daemonize it
-    # p.start()
 
-def before_feature(context, feature):
-    # -- HINT: Recreate a new flaskr client before each feature is executed.
-    use_fixture(flaskr_client, context)
+    http_server = HTTPServer(WSGIContainer(app))
+    http_server.listen(DEFAULT_APP_TCP_PORT)
+
+    ioloop.IOLoop.instance().start()
+
+
+def before_all(context):
+    context.server_thread = Process(target=run_server)
+    context.server_thread.deamon = True
+    context.server_thread.start()
+
+def after_all(context):
+    context.server_thread.terminate()
