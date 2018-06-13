e0ccfb90d07ac183d987f6c8ebfa3f66fc573887
cartrujillo <carla.trujillo@bluetab.net>
2018-06-13 17:09:13 +0200

    modify path endpoint for get uids when external_id is given

--- api/v1/path.py
+++ api/v1/path.py
@@ -2,7 +2,7 @@
 from flask import Blueprint, jsonify, request
 from api.common.parser import parseBoltPathsFlat, parseBoltPathsTree
 from api.common.query import queryPath, getUuidsFromNodes
-from api.common.utils import checkparams, abort
+from api.common.utils import checkparams, abort, checkonlyone
 from api.settings.db import get_neo4j_db
 from api.settings.auth import auth
 
@@ -12,19 +12,26 @@ path = Blueprint('path', __name__)
 @path.route('/path', methods=['POST'])
 @auth.login_required
 def index():
+    error, param = checkonlyone(["uuids", "titles"], request)
+    if error:
+        return abort(400, {'error': error})
     error = checkparams(["toplevel", "levels", "type_analysis"], request)
     if error:
         return abort(400, {'error': error})
     toplevel = request.json['toplevel']
     levels = request.json['levels']
     type_analysis = request.json['type_analysis']
-    ids = request.json["uuids"]
+    if param == "titles":
+        with get_neo4j_db() as session:
+            ids = session.write_transaction(getUuidsFromNodes, ("external_id", request.json[param]))
+            paths = parseBoltPathsFlat(
+                session.write_transaction(queryPath, type_analysis, toplevel, ids, levels), type_analysis, toplevel, session)
+            return jsonify({"data": {"paths": paths, "uuids": ids}}), 200
 
+    ids = request.json["uuids"]
     with get_neo4j_db() as session:
         paths = parseBoltPathsFlat(
-            session.write_transaction(queryPath, type_analysis,
-                                      toplevel, ids, levels),
-            type_analysis, toplevel, session)
+            session.write_transaction(queryPath, type_analysis, toplevel, ids, levels), type_analysis, toplevel, session)
         return jsonify({"data": {"paths": paths, "uuids": ids}}), 200
 
 
