c67b9eb1875e311daf198edd7c70c67681831cfa
Tom Crossland <tom.crossland@bluetab.net>
2018-09-05 13:03:30 +0200

    chore: disable deployment to old environment

--- create_secret_configuration.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sed -i -e "s/NEO4J_PASSWORD = .*/NEO4J_PASSWORD = \"$NEO4J_PASSWORD\"/g" ./api/settings/config.py
-sed -i -e "s/REDIS_URI = .*/REDIS_URI = \"$REDIS_URI\"/g" ./api/settings/config.py
-sed -i -e "s/SECRET_KEY = .*/SECRET_KEY = \"$GUARDIAN_SECRET_KEY\"/g" ./api/settings/config.py
