6f216bf80f32bfed01e40527d13dad0c4cda9a3b
Nacho <ignacio.hidalgo@bluetab.net>
2018-09-12 12:57:36 +0200

    chore: delete file version and change file init

--- api/__init__.py
+++ api/__init__.py
@@ -1,1 +1,1 @@
-from api.app import app
+
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def get_requirements(key):
 
 setup(
     name='td_dl',
-    version='1.0.0',
+    version='1.4.0',
     description='Api for data lineage on neo4j',
     author='bluetab',
     author_email='bluetab@bluetab.net',
