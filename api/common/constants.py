65bf919e749630c3cbafd460ac9291b0cab5e667
Tom Crossland <tom.crossland@bluetab.net>
2019-03-11 22:33:08 +0100

    fix: query issue

--- api/common/constants.py
+++ api/common/constants.py
@@ -1,8 +1,5 @@
-PORT_DES = 4003
-PORT_PRO = 4003
-
 LINEAGE_ANALYSIS_LEVELS = "(r:Resource)<-[D:DEPENDS{levels}]-(n:Resource)"
 IMPACT_ANALYSIS_LEVELS = "(r:Resource)-[D:DEPENDS{levels}]->(n:Resource)"
 
-LINEAGE_ANALYSIS = "(r_ini:Resource)<-[DEPENDS]-(r:Resource)"
-IMPACT_ANALYSIS = "(r_ini:Resource)-[DEPENDS]->(r:Resource)"
+LINEAGE_ANALYSIS = "(r_ini:Resource)<-[:DEPENDS]-(r:Resource)"
+IMPACT_ANALYSIS = "(r_ini:Resource)-[:DEPENDS]->(r:Resource)"
