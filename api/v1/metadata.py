896383f4e5ec80686e23373fee683fcad5f211f9
tom.crossland@bluetab.net <tom.crossland@bluetab.net>
2019-06-28 15:39:39 +0000

    refactor: cache external_id's in batches of 100 (performance)

--- api/v1/metadata.py
+++ api/v1/metadata.py
@@ -3,8 +3,7 @@ from flask import Blueprint, request, jsonify
 from werkzeug.utils import secure_filename
 from api.settings.auth import auth
 from api.settings.db import get_neo4j_db
-from api.common.cache import (get_redis_client, remove_cache,
-                              cache_field_external_ids)
+from api.common.cache import cache_field_external_ids
 from api.common.query import queryExtenalIds
 
 from api.common.utils import abort
@@ -67,10 +66,7 @@ def checkFiles(list_file_request, type_file):
 def cache():
     with get_neo4j_db() as session:
         result = session.read_transaction(queryExtenalIds)
-        client = get_redis_client()
-        remove_cache(client)
         # Cache external_ids with format <system>.<group>.<name>.<field>
         external_ids = [r["external_id"] for r in result if r["external_id"].count(".") == 3]
         cache_field_external_ids(external_ids)
-
     return '', 204
